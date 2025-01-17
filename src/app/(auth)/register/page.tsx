31f59baf53cc2e7f938f70381e404a90503fd9ea
Eldovyn <farras.pramudita@gmail.com>
2025-01-17 21:13:51 +0700

    feat(page): add register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,32 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const RegisterPage = () => {
+    return (
+        <>
+            <div className="bg-[#0b0d14] h-screen flex items-center justify-center w-full text-white">
+                <h1 className="bg-[#12141e] w-[50%] p-8 rounded-md mt-9 border-[#1f2236] border-2">
+                    <p className="text-2xl font-bold">Create Your Account</p>
+                    <form action="">
+                        <div className="flex flex-col mb-3 mt-3">
+                            <Input type="text" placeholder="username" />
+                        </div>
+                        <div className="flex flex-col mb-3">
+                            <Input type="text" placeholder="email" />
+                        </div>
+                        <div className="flex flex-col mb-3">
+                            <Input type="password" placeholder="password" />
+                        </div>
+                        <Button className="bg-[#4b5fe2] hover:bg-[#4558cf] w-full">Register</Button>
+                        <div className="flex flex-row mt-3 text-sm">
+                            <p className="text-[#b5b7da]">have an account?</p>
+                            <p className="ms-1 text-blue-600">Login</p>
+                        </div>
+                    </form>
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default RegisterPage
