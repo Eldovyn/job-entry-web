8f456b81533ffb03c979346414479ba8545c3879
Eldovyn <farras.pramudita@gmail.com>
2025-01-19 23:12:23 +0700

    fix(profile): update sidebar profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import Sidebar from "@/layout/SideBar";
+import SideBar from "@/components/SideBar";
 
 const Profile = () => {
-    return <Sidebar category="profile" />
+    return (
+        <>
+            <div className="flex">
+                <SideBar />
+                <main className="flex-1 ml-20 sm:ml-40 lg:ml-72 p-8">
+                    <p className="text-2xl text-red-600">Profile</p>
+                </main>
+            </div>
+        </>
+    )
 };
 
 export default Profile;
