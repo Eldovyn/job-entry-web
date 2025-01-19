75d2bb7d9d5138ab28b61874b32fc686007c387f
Eldovyn <farras.pramudita@gmail.com>
2025-01-20 00:28:33 +0700

    refactor(sidebar): replace react-responsive with TailwindCSS media query utilities

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { IoHomeOutline } from "react-icons/io5";
 import { VscAccount } from "react-icons/vsc";
-import { useMediaQuery } from 'react-responsive';
 import IconProject from '../../public/IconRemoverBg.png';
 import Avatar from '../../public/avatar.jpg';
 
@@ -13,36 +12,34 @@ const SideBar: React.FC = () => {
     const pathname = usePathname();
     const [isCollapsed, setIsCollapsed] = useState(false);
 
-    const isSm = useMediaQuery({ query: '(max-width: 640px)' });
     const isActive = (path: string) => pathname === path;
 
     return (
         <div
-            className={`fixed top-0 left-0 h-screen bg-[#0b0d14] text-white border-r-2 border-[#1f2236] transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-20 sm:w-40 lg:w-72'
-                }`}
+            className={`fixed top-0 left-0 h-screen bg-[#0b0d14] text-white border-r-2 border-[#1f2236] transition-all duration-300 ${
+                isCollapsed ? 'w-20' : 'w-20 sm:w-40 lg:w-72'
+            }`}
         >
-            {isSm ? (
-                <Image src={IconProject} alt="Logo" className={`w-[45px] mt-5 ${isSm && 'ms-4'}`} />
-            ) : (
-                <>
-                    <div className="flex flex-row mt-3 p-5 justify-center border-b-2 border-[#1f2236]">
-                        <Image src={IconProject} alt="Logo" width={50} />
-                        <p className='ml-1 font-semibold'>Eldovyn</p>
+            <div className="sm:hidden mr-4 flex items-center justify-center mt-5">
+                <Image src={IconProject} alt="Logo" className="w-[45px] ms-4" />
+            </div>
+            <div className="hidden sm:block">
+                <div className="flex flex-row mt-3 p-5 justify-center border-b-2 border-[#1f2236]">
+                    <Image src={IconProject} alt="Logo" width={50} />
+                    <p className="ml-1 font-semibold">Eldovyn</p>
+                </div>
+                <br />
+            </div>
+
+            <div className="hidden sm:block">
+                <div className="flex flex-col justify-center border-b-2 border-[#1f2236] pb-5">
+                    <div className="flex flex-col mb-3 ms-1 items-center justify-center">
+                        <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
                     </div>
-                    <br />
-                </>
-            )}
-            {!isSm && (
-                <>
-                    <div className="flex flex-col justify-center border-b-2 border-[#1f2236] pb-5">
-                        <div className="flex flex-col mb-3 ms-1 items-center justify-center">
-                            <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
-                        </div>
-                        <p className='ml-1 text-sm text-center'>Andana Farras Pramudita</p>
-                    </div>
-                    <br />
-                </>
-            )}
+                    <p className="ml-1 text-sm text-center">Andana Farras Pramudita</p>
+                </div>
+                <br />
+            </div>
 
             <ul className="text-white mt-3 ms-1 overflow-hidden">
                 <li>
@@ -54,10 +51,8 @@ const SideBar: React.FC = () => {
                                 : ''
                         }`}
                     >
-                        <IoHomeOutline size={22} className={`${isSm && 'ms-3'}`} />
-                        {!isCollapsed && (
-                            <p className="text-sm hidden sm:block lg:block">Home</p>
-                        )}
+                        <IoHomeOutline size={22} className="ms-3" />
+                        <p className="text-sm hidden sm:block lg:block">Home</p>
                     </Link>
                 </li>
                 <li>
@@ -69,10 +64,8 @@ const SideBar: React.FC = () => {
                                 : ''
                         }`}
                     >
-                        <VscAccount size={22} className={`${isSm && 'ms-3'}`} />
-                        {!isCollapsed && (
-                            <p className="text-sm hidden sm:block lg:block">Profile</p>
-                        )}
+                        <VscAccount size={22} className="ms-3" />
+                        <p className="text-sm hidden sm:block lg:block">Profile</p>
                     </Link>
                 </li>
             </ul>
