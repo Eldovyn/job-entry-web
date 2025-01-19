c04ad8bb5088367d3cf7d40480c98abb9a1cef72
Eldovyn <farras.pramudita@gmail.com>
2025-01-20 00:51:35 +0700

    style(sidebar): add a border below the project icon

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -20,36 +20,36 @@ const SideBar: React.FC = () => {
                 isCollapsed ? 'w-20' : 'w-20 sm:w-40 lg:w-72'
             }`}
         >
-            <div className="sm:hidden mr-4 flex items-center justify-center mt-5">
-                <Image src={IconProject} alt="Logo" className="w-[45px] ms-4" />
+            <div className="flex items-center justify-center mt-5 sm:hidden mr-1">
+                <Image src={IconProject} alt="Logo" className="w-[45px]" />
             </div>
+
+            <div className="sm:hidden mt-4 border-b-2 border-[#1f2236]" />
+
             <div className="hidden sm:block">
                 <div className="flex flex-row mt-3 p-5 justify-center border-b-2 border-[#1f2236]">
                     <Image src={IconProject} alt="Logo" width={50} />
                     <p className="ml-1 font-semibold">Eldovyn</p>
                 </div>
-                <br />
             </div>
 
-            <div className="hidden sm:block">
+            <div className="hidden sm:block mt-5">
                 <div className="flex flex-col justify-center border-b-2 border-[#1f2236] pb-5">
                     <div className="flex flex-col mb-3 ms-1 items-center justify-center">
                         <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
                     </div>
                     <p className="ml-1 text-sm text-center">Andana Farras Pramudita</p>
                 </div>
-                <br />
             </div>
 
             <ul className="text-white mt-3 ms-1 overflow-hidden">
                 <li>
                     <Link
                         href="#"
-                        className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer rounded-sm ${
-                            isActive('/dasbor-admin')
-                                ? 'border border-primary bg-red-700'
-                                : ''
-                        }`}
+                        className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer rounded-sm ${isActive('/dasbor-admin')
+                            ? 'border border-primary bg-red-700'
+                            : ''
+                            }`}
                     >
                         <IoHomeOutline size={22} className="ms-3" />
                         <p className="text-sm hidden sm:block lg:block">Home</p>
@@ -58,11 +58,10 @@ const SideBar: React.FC = () => {
                 <li>
                     <Link
                         href="#"
-                        className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer rounded-sm ${
-                            isActive('/profile')
-                                ? 'border border-primary bg-[#4b5fe2]'
-                                : ''
-                        }`}
+                        className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer rounded-sm ${isActive('/profile')
+                            ? 'border border-primary bg-[#4b5fe2]'
+                            : ''
+                            }`}
                     >
                         <VscAccount size={22} className="ms-3" />
                         <p className="text-sm hidden sm:block lg:block">Profile</p>
