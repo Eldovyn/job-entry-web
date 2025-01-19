d7aa156cdaff4790b2249f79cd5fe1b8273fb5ed
Eldovyn <farras.pramudita@gmail.com>
2025-01-19 15:24:36 +0700

    feat(layout): add layout sidebar

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -1,12 +1,15 @@
 'use client';
-
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import Gambar from '/public/people_icon.png';
 
-const Sidebar = () => {
+interface SidebarProps {
+    category: String;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ category }) => {
     const pathname = usePathname();
     const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -15,13 +18,11 @@ const Sidebar = () => {
     return (
         <div className="flex">
             <div
-                className={`fixed top-0 left-0 h-screen transition-all duration-300 bg-white border-r ${
-                    isCollapsed
+                className={`fixed top-0 left-0 h-screen bg-white border-r transition-all duration-300 ${isCollapsed
                         ? 'w-20'
-                        : 'w-20 sm:w-40 md:w-40 xl:w-72 lg:w-72'
-                }`}
+                        : 'w-20 sm:w-40 md:w-40 lg:w-72 xl:w-72'
+                    }`}
             >
-                {/* Toggle Button */}
                 <button
                     onClick={() => setIsCollapsed(!isCollapsed)}
                     className="p-2 m-3 bg-gray-200 rounded-full hover:bg-gray-300"
@@ -33,47 +34,43 @@ const Sidebar = () => {
                     <li>
                         <Link
                             href="/dasbor-admin"
-                            className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${
-                                isActive('/dasbor-admin')
+                            className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/dasbor-admin')
                                     ? 'bg-[#F2F3FF] border-r-4 border-primary'
                                     : ''
-                            }`}
+                                }`}
                         >
                             <Image src={Gambar} alt="Dasbor Icon" width={24} height={24} />
                             {!isCollapsed && (
-                                <p className="hidden sm:block md:hidden lg:block">Dasbor</p>
+                                <p className="hidden sm:block lg:block">Dasbor</p>
                             )}
                         </Link>
                     </li>
                     <li>
                         <Link
                             href="/janji-temu"
-                            className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${
-                                isActive('/janji-temu')
+                            className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/janji-temu')
                                     ? 'bg-[#F2F3FF] border-r-4 border-primary'
                                     : ''
-                            }`}
+                                }`}
                         >
                             <Image src={Gambar} alt="Janji Temu Icon" width={24} height={24} />
                             {!isCollapsed && (
-                                <p className="hidden sm:block md:hidden lg:block">Janji Temu</p>
+                                <p className="hidden sm:block lg:block">Janji Temu</p>
                             )}
                         </Link>
                     </li>
-                    {/* Tambahkan item menu lainnya */}
                 </ul>
             </div>
-
-            {/* Content Section */}
             <div
-                className={`flex-1 ml-20 sm:ml-72 md:ml-40 xl:ml-72 lg:ml-72 p-8 overflow-y-auto`}
+                className={`flex-1 ml-20 sm:ml-40 md:ml-40 lg:ml-72 xl:ml-72 p-8 overflow-y-auto`}
             >
-                <h1 className="text-xl font-bold">Content Area</h1>
-                <p>This is the content area. Here you can add your page content.</p>
-                {/* Tambahkan lebih banyak konten untuk melihat efek scroll */}
-                {Array.from({ length: 50 }, (_, i) => (
-                    <p key={i}>Content line {i + 1}</p>
-                ))}
+                {
+                    category === 'coba' ? (
+                        <>
+                            <p>Coba</p>
+                        </>
+                    ) : ''
+                }
             </div>
         </div>
     );
