99d693caba53b74f0cf69d878b21dfd42423de28
Eldovyn <farras.pramudita@gmail.com>
2025-01-19 15:33:22 +0700

    fix(sidebar): fix open sidebar at mobile responsive

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Sidebar from "@/layout/SideBar";
+
+const Profile = () => {
+    return <Sidebar category="profile" />
+};
+
+export default Profile;
--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import Gambar from '/public/people_icon.png';
+import { useMediaQuery } from 'react-responsive'
 
 interface SidebarProps {
     category: String;
@@ -13,15 +14,19 @@ const Sidebar: React.FC<SidebarProps> = ({ category }) => {
     const pathname = usePathname();
     const [isCollapsed, setIsCollapsed] = useState(false);
 
+    const isSm = useMediaQuery({
+        query: '(max-width: 640px)'
+    })
+
     const isActive = (path: string) => pathname === path;
 
     return (
         <div className="flex">
             <div
                 className={`fixed top-0 left-0 h-screen bg-white border-r transition-all duration-300 ${isCollapsed
-                        ? 'w-20'
-                        : 'w-20 sm:w-40 md:w-40 lg:w-72 xl:w-72'
-                    }`}
+                    ? 'w-20'
+                    : 'w-20 sm:w-40 md:w-40 lg:w-72 xl:w-72'
+                    } ${isSm && isCollapsed ? 'w-full' : ''}`}
             >
                 <button
                     onClick={() => setIsCollapsed(!isCollapsed)}
@@ -35,8 +40,8 @@ const Sidebar: React.FC<SidebarProps> = ({ category }) => {
                         <Link
                             href="/dasbor-admin"
                             className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/dasbor-admin')
-                                    ? 'bg-[#F2F3FF] border-r-4 border-primary'
-                                    : ''
+                                ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                : ''
                                 }`}
                         >
                             <Image src={Gambar} alt="Dasbor Icon" width={24} height={24} />
@@ -49,8 +54,8 @@ const Sidebar: React.FC<SidebarProps> = ({ category }) => {
                         <Link
                             href="/janji-temu"
                             className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/janji-temu')
-                                    ? 'bg-[#F2F3FF] border-r-4 border-primary'
-                                    : ''
+                                ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                : ''
                                 }`}
                         >
                             <Image src={Gambar} alt="Janji Temu Icon" width={24} height={24} />
@@ -65,7 +70,7 @@ const Sidebar: React.FC<SidebarProps> = ({ category }) => {
                 className={`flex-1 ml-20 sm:ml-40 md:ml-40 lg:ml-72 xl:ml-72 p-8 overflow-y-auto`}
             >
                 {
-                    category === 'coba' ? (
+                    category === 'profile' ? (
                         <>
                             <p>Coba</p>
                         </>
