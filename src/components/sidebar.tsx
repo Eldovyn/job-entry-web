3c84ed7f5b08aaf32da26f85604bc5ab018583e8
Eldovyn <farras.pramudita@gmail.com>
2025-01-20 00:24:14 +0700

    fix(sidebar): update responsive icon sidebar & active page

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -21,7 +21,9 @@ const SideBar: React.FC = () => {
             className={`fixed top-0 left-0 h-screen bg-[#0b0d14] text-white border-r-2 border-[#1f2236] transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-20 sm:w-40 lg:w-72'
                 }`}
         >
-            {isSm ? <Image src={IconProject} alt="Logo" className={`w-[45px] mt-5 ${isSm && 'ms-3'}`} /> : (
+            {isSm ? (
+                <Image src={IconProject} alt="Logo" className={`w-[45px] mt-5 ${isSm && 'ms-4'}`} />
+            ) : (
                 <>
                     <div className="flex flex-row mt-3 p-5 justify-center border-b-2 border-[#1f2236]">
                         <Image src={IconProject} alt="Logo" width={50} />
@@ -30,50 +32,50 @@ const SideBar: React.FC = () => {
                     <br />
                 </>
             )}
-            {
-                !isSm && (
-                    <>
-                        <div className="flex flex-col justify-center border-b-2 border-[#1f2236] pb-5">
-                            <div className="flex flex-col mb-3 ms-1 items-center justify-center">
-                                <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
-                            </div>
-                            <p className='ml-1 text-sm text-center'>Andana Farras Pramudita</p>
+            {!isSm && (
+                <>
+                    <div className="flex flex-col justify-center border-b-2 border-[#1f2236] pb-5">
+                        <div className="flex flex-col mb-3 ms-1 items-center justify-center">
+                            <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
                         </div>
-                        <br />
-                    </>
-                )
-            }
+                        <p className='ml-1 text-sm text-center'>Andana Farras Pramudita</p>
+                    </div>
+                    <br />
+                </>
+            )}
 
-            <ul className="text-white mt-3 ms-1">
-                    <li>
-                        <Link
-                            href="#"
-                            className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/dasbor-admin')
-                                ? 'border-r-4 border-primary'
+            <ul className="text-white mt-3 ms-1 overflow-hidden">
+                <li>
+                    <Link
+                        href="#"
+                        className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer rounded-sm ${
+                            isActive('/dasbor-admin')
+                                ? 'border border-primary bg-red-700'
                                 : ''
-                                }`}
-                        >
-                            <IoHomeOutline size={22} className={`${isSm && 'ms-2'}`}/>
-                            {!isCollapsed && (
-                                <p className="text-sm hidden sm:block lg:block">Home</p>
-                            )}
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="#"
-                            className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/janji-temu')
-                                ? 'border-r-4 border-primary'
+                        }`}
+                    >
+                        <IoHomeOutline size={22} className={`${isSm && 'ms-3'}`} />
+                        {!isCollapsed && (
+                            <p className="text-sm hidden sm:block lg:block">Home</p>
+                        )}
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="#"
+                        className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer rounded-sm ${
+                            isActive('/profile')
+                                ? 'border border-primary bg-[#4b5fe2]'
                                 : ''
-                                }`}
-                        >
-                            <VscAccount size={22} className={`${isSm && 'ms-2'}`}/>
-                            {!isCollapsed && (
-                                <p className="text-sm hidden sm:block lg:block">Profile</p>
-                            )}
-                        </Link>
-                    </li>
-                </ul>
+                        }`}
+                    >
+                        <VscAccount size={22} className={`${isSm && 'ms-3'}`} />
+                        {!isCollapsed && (
+                            <p className="text-sm hidden sm:block lg:block">Profile</p>
+                        )}
+                    </Link>
+                </li>
+            </ul>
         </div>
     );
 };
