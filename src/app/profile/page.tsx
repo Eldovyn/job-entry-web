3104ce72321e2fe95e4198eca91fa23ab43f863f
Eldovyn <farras.pramudita@gmail.com>
2025-01-20 00:04:05 +0700

    fix(profile): update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import SideBar from "@/components/sidebar";
+import Avatar from '../../../public/avatar.jpg';
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
     return (
@@ -7,54 +10,11 @@ const Profile = () => {
             <div className="flex">
                 <SideBar />
                 <main className="flex-1 ml-20 sm:ml-40 lg:ml-72 p-8 bg-[#0b0d14]">
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
-                    <p className="text-2xl text-red-600">Profile</p>
+                    <div className="bg-[#12141e] min-h-screen p-5 rounded-md border-[#1f2236] border-2 flex flex-col">
+                        <p className="text-lg mb-3 text-white">Profile Picture</p>
+                        <Image src={Avatar} alt="User Icon" width={100} height={100} className="rounded-full" />
+                        <Button className="bg-[#4b5fe2] hover:bg-[#4558cf] w-[10rem] mt-5">Upload Avatar</Button>
+                    </div>
                 </main>
             </div>
         </>
