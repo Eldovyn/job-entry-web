3b08b5a883bce7b20a74e7f7fac00f8ac9242a26
Eldovyn <farras.pramudita@gmail.com>
2025-01-19 17:07:05 +0700

    fix(sidebar): update responsive design

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -3,8 +3,11 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
-import Gambar from '/public/people_icon.png';
+import { IoHomeOutline } from "react-icons/io5";
+import { VscAccount } from "react-icons/vsc";
 import { useMediaQuery } from 'react-responsive'
+import IconProject from '../../public/IconRemoverBg.png';
+import Avatar from '../../public/avatar.jpg';
 
 interface SidebarProps {
     category: String;
@@ -23,44 +26,67 @@ const Sidebar: React.FC<SidebarProps> = ({ category }) => {
     return (
         <div className="flex">
             <div
-                className={`fixed top-0 left-0 h-screen bg-white border-r transition-all duration-300 ${isCollapsed
+                className={`fixed top-0 left-0 h-screen text-white bg-[#0b0d14] border-r transition-all duration-300 ${isCollapsed
                     ? 'w-20'
                     : 'w-20 sm:w-40 md:w-40 lg:w-72 xl:w-72'
                     } ${isSm && isCollapsed ? 'w-full' : ''}`}
             >
-                <button
+                {/* <button
                     onClick={() => setIsCollapsed(!isCollapsed)}
                     className="p-2 m-3 bg-gray-200 rounded-full hover:bg-gray-300"
                 >
                     {isCollapsed ? '➡️' : '⬅️'}
-                </button>
-
-                <ul className="text-[#515151] mt-5">
+                </button> */}
+                {isSm ? <Image src={IconProject} alt="Logo" className={`w-[45px] mt-5 ${isSm && 'ms-3'}`} /> : (
+                    <>
+                        <div className="flex flex-row mt-3 p-5 justify-center">
+                            <Image src={IconProject} alt="Logo" width={50} />
+                            <p className='ml-1 font-semibold'>Eldovyn</p>
+                        </div>
+                        <hr className='border-[#1b1d2e] border-1'/>
+                        <br />
+                    </>
+                )}
+                {
+                    !isSm && (
+                        <>
+                            <div className="flex flex-col justify-center">
+                                <div className="flex flex-col mb-3 ms-1 items-center justify-center">
+                                    <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
+                                </div>
+                                <p className='ml-1 text-sm text-center'>Andana Farras Pramudita</p>
+                            </div>
+                            <br />
+                            <hr className='border-[#1b1d2e] border-1'/>
+                        </>
+                    )
+                }
+                <ul className="text-white mt-5 ms-1">
                     <li>
                         <Link
-                            href="/dasbor-admin"
+                            href="#"
                             className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/dasbor-admin')
-                                ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                ? 'border-r-4 border-primary'
                                 : ''
                                 }`}
                         >
-                            <Image src={Gambar} alt="Dasbor Icon" width={24} height={24} />
+                            <IoHomeOutline size={22} className={`${isSm && 'ms-2'}`}/>
                             {!isCollapsed && (
-                                <p className="hidden sm:block lg:block">Dasbor</p>
+                                <p className="text-sm hidden sm:block lg:block">Home</p>
                             )}
                         </Link>
                     </li>
                     <li>
                         <Link
-                            href="/janji-temu"
+                            href="#"
                             className={`flex items-center gap-3 py-3.5 px-3 md:px-5 cursor-pointer ${isActive('/janji-temu')
-                                ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                ? 'border-r-4 border-primary'
                                 : ''
                                 }`}
                         >
-                            <Image src={Gambar} alt="Janji Temu Icon" width={24} height={24} />
+                            <VscAccount size={22} className={`${isSm && 'ms-2'}`}/>
                             {!isCollapsed && (
-                                <p className="hidden sm:block lg:block">Janji Temu</p>
+                                <p className="text-sm hidden sm:block lg:block">Profile</p>
                             )}
                         </Link>
                     </li>
