8aaf9cad017cf068563e0df2c194d75ef1b24950
Eldovyn <farras.pramudita@gmail.com>
2025-01-19 23:46:04 +0700

    fix(sidebar): update sidebar

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,59 @@
 import React from "react";
-import SideBar from "@/components/SideBar";
+import SideBar from "@/components/sidebar";
 
 const Profile = () => {
     return (
         <>
             <div className="flex">
                 <SideBar />
-                <main className="flex-1 ml-20 sm:ml-40 lg:ml-72 p-8">
+                <main className="flex-1 ml-20 sm:ml-40 lg:ml-72 p-8 bg-[#0b0d14]">
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
+                    <p className="text-2xl text-red-600">Profile</p>
                     <p className="text-2xl text-red-600">Profile</p>
                 </main>
             </div>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -18,36 +18,33 @@ const SideBar: React.FC = () => {
 
     return (
         <div
-            className={`fixed top-0 left-0 h-screen bg-[#0b0d14] text-white border-r transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-20 sm:w-40 lg:w-72'
+            className={`fixed top-0 left-0 h-screen bg-[#0b0d14] text-white border-r-2 border-[#1f2236] transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-20 sm:w-40 lg:w-72'
                 }`}
         >
             {isSm ? <Image src={IconProject} alt="Logo" className={`w-[45px] mt-5 ${isSm && 'ms-3'}`} /> : (
                 <>
-                    <div className="flex flex-row mt-3 p-5 justify-center">
+                    <div className="flex flex-row mt-3 p-5 justify-center border-b-2 border-[#1f2236]">
                         <Image src={IconProject} alt="Logo" width={50} />
                         <p className='ml-1 font-semibold'>Eldovyn</p>
                     </div>
-                    <hr className='border-[#1b1d2e] border-1' />
                     <br />
                 </>
             )}
             {
                 !isSm && (
                     <>
-                        <div className="flex flex-col justify-center">
+                        <div className="flex flex-col justify-center border-b-2 border-[#1f2236] pb-5">
                             <div className="flex flex-col mb-3 ms-1 items-center justify-center">
                                 <Image src={Avatar} alt="User Icon" width={50} height={50} className="rounded-full" />
                             </div>
                             <p className='ml-1 text-sm text-center'>Andana Farras Pramudita</p>
                         </div>
                         <br />
-                        <hr className='border-[#1b1d2e] border-1' />
                     </>
                 )
             }
 
-            {/* Navigation */}
-            <ul className="text-white mt-5 ms-1">
+            <ul className="text-white mt-3 ms-1">
                     <li>
                         <Link
                             href="#"
