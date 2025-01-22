7d0361e53324a3cab35864de34c51ab703da985b
Eldovyn <farras.pramudita@gmail.com>
2025-01-22 18:34:19 +0700

    style(login): update title login at stuff login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -7,7 +7,7 @@ const AdminLogin = () => {
         <>
             <div className="bg-[#0b0d14] h-screen flex items-center justify-center w-full">
                 <div className="bg-[#12141e] w-[25%] border-2 p-8 rounded-md mt-9 border-[#1f2236]">
-                    <p className="text-white text-2xl font-semibold">Login Stuf</p>
+                    <p className="text-white text-2xl font-semibold">Login Stuff</p>
                     <form action="" className="mt-3">
                         <div className="flex flex-col">
                             <Input className="caret-white border-[#1b1d2e] border-2 focus:border-[#4b5fe2]" placeholder="email" type="text" />
