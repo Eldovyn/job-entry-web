b71b4e231161e8dacb54933fd4fdcfe26693a545
Eldovyn <farras.pramudita@gmail.com>
2025-01-25 22:31:05 +0700

    feat(page): add page user form information

---
+++ src/app/admin/dashboard/user/page.tsx
@@ -0,0 +1,9 @@
+const DashboardUser = () => {
+    return (
+        <>
+            <div className="bg-[#0b0d14] h-screen flex items-center justify-center w-full">Dashboard User</div>
+        </>
+    )
+}
+
+export default DashboardUser
