f3a70e16e5cc56286ff8b3b9a3a550f25373e7c4
Eldovyn <farras.pramudita@gmail.com>
2025-01-25 22:18:00 +0700

    feat(dashboard): add table data mahasiswa

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,19 +1,84 @@
 'use client';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { FaSearch } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { IoExitOutline } from "react-icons/io5";
 import { useMediaQuery } from 'react-responsive'
+import Link from "next/link";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+interface DataMahasiswa {
+    name: string;
+    noHp: string;
+    tempatLahir: string;
+    tanggalLahir: string;
+    jenisKelamin: string;
+    npm: string;
+    kelas: string;
+    lokasiKampus: string;
+}
 
 const Dashboard = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
     const isMobile = useMediaQuery({
-        query: '(max-width: 640px)'
+        query: '(max-width: 640px) and (min-width: 320px)'
     })
 
     const isUltraMobile = useMediaQuery({
         query: '(max-width: 320px)'
     })
+
+    const isTablet = useMediaQuery({
+        query: '(min-width: 640px) and (max-width: 1024px)'
+    })
+
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 1024px)'
+    })
+
+    if (!isClient) {
+        return null;
+    }
+
+    const dataMahasiswa: DataMahasiswa[] = [
+        {
+            name: "Andana Farras Pramudita",
+            noHp: "081234567890",
+            tempatLahir: "Bandung",
+            tanggalLahir: "01-01-2000",
+            jenisKelamin: "Laki-laki",
+            npm: "20201001",
+            kelas: "TI-2A",
+            lokasiKampus: "Bandung",
+        },
+        {
+            name: "Andana Farras Pramudita",
+            noHp: "081234567890",
+            tempatLahir: "Bandung",
+            tanggalLahir: "01-01-2000",
+            jenisKelamin: "Laki-laki",
+            npm: "20201001",
+            kelas: "TI-2A",
+            lokasiKampus: "Bandung",
+        },
+        {
+            name: "Andana Farras Pramudita",
+            noHp: "081234567890",
+            tempatLahir: "Bandung",
+            tanggalLahir: "01-01-2000",
+            jenisKelamin: "Laki-laki",
+            npm: "20201001",
+            kelas: "TI-2A",
+            lokasiKampus: "Bandung",
+        }
+    ]
 
     return (
         <>
@@ -25,7 +90,7 @@ const Dashboard = () => {
                     <div className="mt-3 flex justify-end flex-row">
                         <div className="relative lg:w-[30%] w-full flex">
                             <Input
-                                className="caret-white border-[#1b1d2e] border-2 focus:border-[#4b5fe2] pr-10"
+                                className="caret-white border-[#1b1d2e] border-2 focus:border-[#4b5fe2] pr-10 h-[2.2rem]"
                                 placeholder="cari sesuai nama/npm"
                                 type="text"
                             />
@@ -33,9 +98,9 @@ const Dashboard = () => {
                                 <FaSearch />
                             </span>
                         </div>
-                        {!isMobile ? (
+                        {!isMobile && !isUltraMobile ? (
                             <>
-                                <Button className="bg-[#4b5fe2] hover:bg-[#4558cf] ms-3 lg:w-[10%] md:w-[20%] w-[30%] h-[2.05rem]">
+                                <Button className="bg-[#4b5fe2] hover:bg-[#4558cf] ms-3 lg:w-[10%] md:w-[20%] w-[30%] h-[2.2rem]">
                                     <div className="flex flex-row">
                                         <IoExitOutline className="text-white pt-1 pe-1" />
                                         <p className="text-white">Logout</p>
@@ -45,13 +110,69 @@ const Dashboard = () => {
                     </div>
                     {isMobile || isUltraMobile ? (
                         <>
-                            <Button className={`bg-[#4b5fe2] hover:bg-[#4558cf] mt-3 lg:w-[10%] md:w-[20%] ${isMobile ? 'w-[30%]' : ''} ${isUltraMobile ? 'w-[70%]' : ''} h-[2.05rem]`}>
+                            <Button className={`bg-[#4b5fe2] hover:bg-[#4558cf] mt-3 lg:w-[10%] md:w-[20%] ${isMobile ? 'w-[40%]' : ''} ${isUltraMobile ? 'w-[70%]' : ''} h-[2.05rem]`}>
                                 <div className="flex flex-row">
                                     <IoExitOutline className="text-white pt-1 pe-1" />
                                     <p className="text-white">Logout</p>
                                 </div>
                             </Button>
                         </>) : ''}
+                    {isDesktop ? (
+                        <>
+                            <table className="table-auto w-full border-2 mt-2 text-white text-center">
+                                <thead className="bg-[#1f2236]">
+                                    <tr>
+                                        <th className="border-2 border-[#1f2236] px-4 py-2">Action</th>
+                                        <th className="border-2 border-[#1f2236] px-4 py-2">Nama</th>
+                                        <th className="border-2 border-[#1f2236] px-4 py-2">NPM</th>
+                                        <th className="border-2 border-[#1f2236] px-4 py-2">Kelas</th>
+                                        <th className="border-2 border-[#1f2236] px-4 py-2">Lokasi Kampus</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="text-center">
+                                    {dataMahasiswa.map((mahasiswa, index) => (
+                                        <tr key={index}>
+                                            <td className="border-2 border-[#1f2236] px-4 py-2">
+                                                <div className="flex flex-row justify-center">
+                                                    <div className="border p-2 rounded-md bg-[#4b5fe2] border-[#1f2236] me-1">
+                                                        <MdDelete className="cursor-pointer text-red-500" size={20} />
+                                                    </div>
+                                                    <Link href={`/admin/dashboard/${mahasiswa.npm}`}>
+                                                        <div className="border p-2 rounded-md bg-[#4b5fe2] border-[#1f2236] ms-1">
+                                                            <FaExternalLinkAlt className="cursor-pointer text-white" size={20} />
+                                                        </div>
+                                                    </Link>
+                                                </div>
+                                            </td>
+                                            <td className="border-2 border-[#1f2236] px-4 py-2">{mahasiswa.name}</td>
+                                            <td className="border-2 border-[#1f2236] px-4 py-2">{mahasiswa.npm}</td>
+                                            <td className="border-2 border-[#1f2236] px-4 py-2">{mahasiswa.kelas}</td>
+                                            <td className="border-2 border-[#1f2236] px-4 py-2">{mahasiswa.lokasiKampus}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </>) : ''}
+                    {isTablet ? (
+                        dataMahasiswa.map((mahasiswa, index) => (
+                            <>
+                                <div className="border rounded-md border-[#1f2236] mt-2 p-3 flex justify-between items-center text-white">
+                                    <div className="flex flex-row justify-center">
+                                        <div className="border p-2 rounded-md bg-[#4b5fe2] border-[#1f2236] me-1">
+                                            <MdDelete className="cursor-pointer text-red-500" size={20} />
+                                        </div>
+                                        <Link href={`/admin/dashboard/${mahasiswa.npm}`}>
+                                            <div className="border p-2 rounded-md bg-[#4b5fe2] border-[#1f2236] ms-1">
+                                                <FaExternalLinkAlt className="cursor-pointer text-white" size={20} />
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <p className="text-center">{mahasiswa.name}</p>
+                                    <p className="text-center">{mahasiswa.npm}</p>
+                                </div>
+                            </>
+                        ))
+                    ) : ''}
                 </div>
             </div>
         </>
