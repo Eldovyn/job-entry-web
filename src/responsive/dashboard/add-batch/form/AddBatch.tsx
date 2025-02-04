578023cb3e67cee2524d3f805772cbe6008d2580
Eldovyn <farras.pramudita@gmail.com>
2025-02-04 23:52:22 +0700

    feat(open): add integration api batch pendaftaran

--- src/responsive/dashboard/add-batch/form/AddBatch.tsx
+++ src/responsive/dashboard/add-batch/form/AddBatch.tsx
@@ -1,6 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useMutation } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
+import { AxiosError } from "axios";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
 import React from "react";
+import { useFormik } from "formik";
+import Cookies from "js-cookie";
+import LoadingSpinnerComponent from "react-spinners-components";
 
 interface Props {
     isDesktop: boolean;
@@ -8,11 +16,99 @@ interface Props {
     isMobile: boolean;
 }
 
+interface FormData {
+    title: string;
+    description: string;
+}
+
+interface FormErrors {
+    title: string[];
+    description: string[];
+}
+
+interface ErrorResponse {
+    message: string;
+    errors?: {
+        [field: string]: string[];
+    };
+}
+
 const AddBatch: React.FC<Props> = ({ isDesktop, isTablet, isMobile }) => {
     if (!isDesktop && !isTablet && !isMobile) return null;
 
+    const { toast } = useToast();
+
+    const [formErrors, setFormErrors] = useState<FormErrors>({
+        title: [],
+        description: [],
+    });
+
+    const handleValidation = (errors: { title: string[]; description: string[] }) => {
+        setFormErrors({
+            title: errors.title || [],
+            description: errors.description || [],
+        });
+    };
+
+    const { mutate } = useMutation({
+        mutationFn: async (data: FormData) => {
+            const response = await axiosInstance.post('/job-entry/admin/batch', data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${Cookies.get('accessToken') || ''}`
+                },
+            });
+            return response;
+        },
+        onError: (error) => {
+            const err = error as AxiosError<ErrorResponse>;
+            if (err.response?.status === 400 && err.response.data.errors) {
+                handleValidation({
+                    title: err.response?.data?.errors?.title ?? [],
+                    description: err.response?.data?.errors?.description ?? [],
+                });
+            }
+            toast({
+                description: err?.response?.data?.message,
+            })
+            return
+        },
+        onSuccess: async (data) => {
+            const dataApi = data.data
+            toast({
+                description: dataApi.message,
+            })
+            handleValidation({
+                title: [],
+                description: [],
+            })
+            formik.setFieldValue('title', '');
+            formik.setFieldValue('description', '');
+        },
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: '',
+        },
+        onSubmit: (values, { setSubmitting }) => {
+            try {
+                const { title, description } = values
+                mutate({
+                    title,
+                    description
+                })
+            } catch (error) {
+                console.error('Terjadi kesalahan:', error);
+            } finally {
+                setSubmitting(false);
+            }
+        },
+    })
+
     return (
-        <form action="" className="mt-3">
+        <form onSubmit={formik.isSubmitting ? () => { } : formik.handleSubmit} className="mt-3">
             {['title', 'description'].map((label, index) => (
                 <div key={index} className="flex flex-col gap-2 pt-2">
                     <label className="text-sm font-semibold text-white">
@@ -21,8 +117,13 @@ const AddBatch: React.FC<Props> = ({ isDesktop, isTablet, isMobile }) => {
                     <Input
                         type="text"
                         placeholder={label}
+                        name={label}
+                        onChange={formik.handleChange} value={formik.values[label as keyof FormData]}
                         className="text-white caret-white border-[#1b1d2e] border-2 focus:border-[#4b5fe2]"
                     />
+                    {formErrors[label as keyof FormErrors].length > 0 && (
+                        <p className="text-sm text-red-500">{formErrors[label as keyof FormErrors][0]}</p>
+                    )}
                 </div>
             ))}
             <div className="flex justify-end">
@@ -30,7 +131,12 @@ const AddBatch: React.FC<Props> = ({ isDesktop, isTablet, isMobile }) => {
                     type="submit"
                     className="mt-3 bg-[#4b5fe2] text-white hover:bg-[#4b5fe2] hover:text-white w-[7rem]"
                 >
-                    Submit
+                    {formik.isSubmitting ? (
+                        <div className="flex flex-row text-white items-center cursor-pointer">
+                            <LoadingSpinnerComponent type={'Spinner'} color={'white'} size={'100px'} />
+                            <p className="ms-1">Add Batch</p>
+                        </div>
+                    ) : "Add Batch"}
                 </Button>
             </div>
         </form>
